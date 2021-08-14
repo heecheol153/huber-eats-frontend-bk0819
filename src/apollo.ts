ebd01ef5a423b26c70b6a6d64a7cf83e8bb61a44
heecheol153 <heecheol153@gmail.com>
2021-08-14 17:26:05 +0900

    #15.0 Local Only Fields

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,23 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client';
+
+export const isLoggedInVar = makeVar(false);
 
 export const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      //Type policy map
+      Query: {
+        fields: {
+          //Field policy map for the Product type
+          isLoggedIn: {
+            read() {
+              //The read function for the isLoggedIn field. read는 field값을 반환하다.
+              return isLoggedInVar();
+            },
+          },
+        },
+      },
+    },
+  }),
 });
