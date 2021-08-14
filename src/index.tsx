f14d3c73c05b9066fc84546e7e0d60dc1b701c7a
heecheol153 <heecheol153@gmail.com>
2021-08-14 09:14:21 +0900

    #14.3 Apollo Setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from '@apollo/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './styles/styles.css';
+import { client } from './apollo';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
