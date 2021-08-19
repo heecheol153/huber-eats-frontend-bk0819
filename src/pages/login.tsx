a9270f596fd5c02dfe65f80f0b871ace75f0a93c
heecheol-untieall <heecheol@untieall.com>
2021-08-19 11:46:58 +0900

    #15.7 Apollo Codegen

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,10 +2,15 @@ import { gql, useMutation } from '@apollo/client';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { FormError } from '../components/form-error';
+import {
+  loginMutation,
+  loginMutationVariables,
+} from '../__generated__/loginMutation';
+//import '../styles/tailwind.css';
 
 //mutation이름은 front-end를 위한것임,back-end로 안넘어간다.
 const LOGIN_MUTATION = gql`
-  mutation PotatoMutation($email: String!, $password: String!) {
+  mutation loginMutation($email: String!, $password: String!) {
     login(input: { email: $email, password: $password }) {
       ok
       token
@@ -25,7 +30,10 @@ export const Login = () => {
     formState: { errors },
     handleSubmit,
   } = useForm<ILoginForm>();
-  const [loginMutation] = useMutation(LOGIN_MUTATION);
+  const [loginMutation, { data }] = useMutation<
+    loginMutation,
+    loginMutationVariables
+  >(LOGIN_MUTATION);
   const onSubmit = () => {
     const { email, password } = getValues();
     loginMutation({
@@ -35,6 +43,7 @@ export const Login = () => {
       },
     });
   };
+  //console.log(data?.login.)
   return (
     <div className='h-screen flex items-center justify-center bg-gray-800'>
       <div className='bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center'>
